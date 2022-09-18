88611b94f37aff7948c28a9a9351dcded56c41f2
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-09-19 00:28:29 +0600

    first commit

---
+++ country.js
@@ -0,0 +1,29 @@
+const loadCountry = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountrys(data));
+}
+const displayCountrys = countrys => {
+    console.log(countrys[0]);
+    const allCountrys = countrys.map(country => getCoutry(country))
+    const container = document.getElementById('countrys');
+    container.innerHTML = allCountrys.join(' ');
+
+}
+
+const getCoutry = country => {
+    return `
+    <div class="d-country">
+    <h1>${country.name.common}</h1>
+    <img src="${country.flags.png}">
+    <h2>${country.capital}</h2>
+    <h3>Flag: ${country.flag}</h3>
+    <h3>Resion: ${country.region}</h3>
+    <h3>Start Of week: ${country.startOfWeek}</h3>
+    <h3>Time: ${country.timezones}</h3>
+    
+    </div>
+    
+    `
+}
+loadCountry();
